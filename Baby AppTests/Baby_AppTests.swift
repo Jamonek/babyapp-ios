3912bf0b1816029852a88116549826727281f128
Jamone Kelly <xg.monie089@gmail.com>
2016-05-12 11:30:32 -0400

    Initial Commit

---
+++ Baby AppTests/Baby_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Baby_AppTests.swift
+//  Baby AppTests
+//
+//  Created by Jamone Alexander Kelly on 5/12/16.
+//  Copyright © 2016 Mehe, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Baby_App
+
+class Baby_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
