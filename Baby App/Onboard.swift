42336aca8e265ad9c693864be45e602ab5601287
Jamone Kelly <xg.monie089@gmail.com>
2016-05-18 12:23:11 -0400

    - Initial commit

---
+++ Baby App/Onboard.swift
@@ -0,0 +1,16 @@
+//
+//  Onboard.swift
+//  Baby App
+//
+//  Created by Jamone Alexander Kelly on 5/17/16.
+//  Copyright © 2016 Mehe, LLC. All rights reserved.
+//
+
+import UIKit
+
+class Onboard: UIViewController {
+    
+    override func viewDidLoad() {
+        //self.view.backgroundColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+    }
+}
